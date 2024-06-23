938cf0608d37e67220fa0811d40cf0ded7bbeafd
Alexei-github <a.beraziuk@student.unsw..edu.au>
2024-06-23 22:21:52 +1000

    attempting to setup `license-webpack-plugin` which throws error on `npm run build` - yet to resolve

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
+import { LicenseWebpackPlugin } from "license-webpack-plugin";
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+// const LicenseWebpackPlugin =
+//   require("license-webpack-plugin").LicenseWebpackPlugin;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (!isServer) {
+      // !dev &&
+      config.plugins.push(
+        new LicenseWebpackPlugin({
+          perChunkOutput: false,
+          outputFilename: "licenses.txt",
+          stats: {
+            warnings: true,
+            errors: true,
+          },
+        })
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
