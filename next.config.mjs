75851f88ecc3d7df9e75ba891aba6e48a2637055
Alexei-github <a.beraziuk@student.unsw..edu.au>
2024-06-24 21:51:01 +1000

    `license-webpack-plugin` configured and cause of its previous error resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,25 @@
-import { LicenseWebpackPlugin } from "license-webpack-plugin";
+//
+
 // import { createRequire } from "module";
 // const require = createRequire(import.meta.url);
-
-// const LicenseWebpackPlugin =
-//   require("license-webpack-plugin").LicenseWebpackPlugin;
+import { LicenseWebpackPlugin } from "license-webpack-plugin";
+// import path from "path";
+// const __dirname = path.resolve();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { dev, isServer }) => {
-    if (!isServer) {
-      // !dev &&
+    if (!isServer && !dev) {
       config.plugins.push(
         new LicenseWebpackPlugin({
-          perChunkOutput: false,
-          outputFilename: "licenses.txt",
+          // perChunkOutput: false,
+          // outputFilename: "licenses.txt",
           stats: {
             warnings: true,
             errors: true,
           },
+          modulesDirectories: ["node_modules"],
+          // modulesDirectories: [path.resolve(__dirname, "node_modules")],
         })
       );
     }
